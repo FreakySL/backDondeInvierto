4be2907a64d8763e19f409f0243d12c4a5e376c8
Santiago Figun <santi.figun@gmail.com>
2024-01-06 20:46:39 -0300

    Se empezo la clase de conexion a la API de Sheets

--- app/sheets/sheet_api.py
+++ app/sheets/sheet_api.py
@@ -2,6 +2,58 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
+
+
+
+class APISpreadsheet:
+
+	SCOPEs = ['https://www.googleapis.com/auth/spreadsheets']
+	KEY = 'key.json'
+	SPREADSHEET_ID = '1EDfxFQA4ncCaRl6G45oyufY8KNhiuFuGRbTb-JGsLzg'
+
+	def __init__(self):
+		self.creds = None
+		self.creds = service_account.Credentials.from_service_account_file(self.KEY, self.SCOPES)
+		self.service = build("sheets","v4",creds)
+		self.sheet = service.spreadsheets()
+
+
+	def get_rows():
+
+		#--------------------------------------------------------
+		#            GET
+		#--------------------------------------------------------
+
+
+		# Llamada a la api
+		result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Hoja 1!A1:H1').execute()
+		# Extraemos values del resultado
+		values = result.get('values',[])
+
+
+
+	def append(self, object : object) -> bool : 
+		"""
+		param:object -> Es cualquier objeto
+		return : booleano (true o false)
+		"""
+		data = object.get_data_to_sheet()
+		
+        values = [data]
+
+        result = sheet.values().append(spreadsheetId=SPREADSHEET_ID,
+									    range='A1',
+									    valueInputOption='USER_ENTERED', 
+										body={'values':values}).execute()
+	
+
+"""
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from pdb import set_trace
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 KEY = 'key.json'
 # Escribe aquí el ID de tu documento:
@@ -19,7 +71,7 @@ sheet = service.spreadsheets()
 
 
 # Llamada a la api
-result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Hoja 1!A1:A8').execute()
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Hoja 1!A1:H6').execute()
 # Extraemos values del resultado
 values = result.get('values',[])
 print(values)
@@ -30,10 +82,13 @@ print(values)
 
 
 # Debe ser una matriz por eso el doble [[]]
-values = [['Prueba!']]
+values = [['Chavo','Chavo2'],['Quico']]
 # Llamamos a la api
+
 result = sheet.values().append(spreadsheetId=SPREADSHEET_ID,
 							range='A1',
 							valueInputOption='USER_ENTERED',
 							body={'values':values}).execute()
 print(f"Datos insertados correctamente.\n{(result.get('updates').get('updatedCells'))}")
+
+"""
