0976531c5788bea504c9583ad0f8e2ce79a0f849
Santiago Figun <santi.figun@gmail.com>
2024-01-07 03:58:03 -0300

    Se agregaron funciones al API SpreadSheet

--- app/sheets/sheet_api.py
+++ app/sheets/sheet_api.py
@@ -12,40 +12,84 @@ class APISpreadsheet:
 	SPREADSHEET_ID = '1EDfxFQA4ncCaRl6G45oyufY8KNhiuFuGRbTb-JGsLzg'
 
 	def __init__(self):
-		self.creds = None
 		self.creds = service_account.Credentials.from_service_account_file(self.KEY, self.SCOPES)
-		self.service = build("sheets","v4",creds)
-		self.sheet = service.spreadsheets()
+		self.service = build("sheets","v4", self.creds)
+		self.sheet = self.service.spreadsheets()
+    
+	def get_all_rows(self):
+		result = self.sheet.values().get(spreadsheetId=self.SPREADSHEET_ID, range='funds!A2:J').execute()
+		array_rows = result.get('values',[])
+		return array_rows
+
+	def post_fund(self, values):
+		response = self.sheet.values().append(
+			spreadsheetId=self.SPREADSHEET_ID, 
+			range='funds!A1', 
+			valueInputOption='USER_ENTERED', 
+			body={'values':values}
+		).execute()
+		print(f"Datos insertados correctamente.\n{(response.get('updates').get('updatedCells'))}")
+
+	def array_row_to_dicctionary(self):
+		return True
+
+    def array_row_to_dicctionary(self):
+        dictionary = {}
+        array_rows = self.get_all_rows()
+
+        for x in array_rows:
+            new = {"class" : x[0], 
+                "fund_name" : x[1], 
+                "trading_currency" : x[2], 
+                "fund_cafci_code" : x[4], 
+                "rescue_time" : x[5], 
+                "risk_level" : x[6], 
+                "tem" : x[7], 
+                "monthly_performance" : x[8], 
+                "updated" : x[9], 
+                "logo_url" : x[10], 
+            }
+
+            dictionary[x[3]] = new
+        return dictionary
 
 
-	def get_rows():
+    def find_new_funds(self, array_row, dictionary):
+        new_funds_array = []
+        for x in array_row:
+            if x[3] in dictionary:
+                continue
+            else:
+                print("Found new fund ID: %s", x[3])
+                new_funds_array.append(x)
+        
+        return new_funds_array
+         
+def post_fund(array_row):
+         
+        result = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range='A1', valueInputOption='USER_ENTERED', body={'values':array_row}).execute()
+        print(f"Datos insertados correctamente.\n{(result.get('updates').get('updatedCells'))}")
+         
 
-		#--------------------------------------------------------
-		#            GET
-		#--------------------------------------------------------
 
 
-		# Llamada a la api
-		result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Hoja 1!A1:H1').execute()
-		# Extraemos values del resultado
-		values = result.get('values',[])
+              
+              
 
 
 
+"""
 	def append(self, object : object) -> bool : 
-		"""
+		
 		param:object -> Es cualquier objeto
 		return : booleano (true o false)
-		"""
+	
 		data = object.get_data_to_sheet()
 		
         values = [data]
 
-        result = sheet.values().append(spreadsheetId=SPREADSHEET_ID,
-									    range='A1',
-									    valueInputOption='USER_ENTERED', 
-										body={'values':values}).execute()
-	
+        result = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range='A1', valueInputOption='USER_ENTERED', body={'values':values}).execute()
+"""
 
 """
 from google.oauth2.credentials import Credentials
